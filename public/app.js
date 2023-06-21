58c70f91cfa97df2b95034d19b7a8b2432ce96a8
Namamish <75522655+Namamish@users.noreply.github.com>
2023-06-21 21:48:32 +0530

    Modifying the Frontend

--- public/app.js
+++ public/app.js
@@ -1,14 +1,38 @@
-const fetchDdata = async () =>
+const fetchUGData = async () =>
 {
-    const res = await axios.get("https://major-programmes.onrender.com/admissions/majorProgramme/visible?category=Undergraduate%20Programme");
+    const res = await axios.get("https://major-programmes.onrender.com/admissions/majorProgramme/visible?category=Undergraduate Programme");
     const records = res.data;
     console.log(records);
+    
+    if(records[0].isComment === true)
+    {
+        const p = document.querySelector("p");
+        p.append(records[0].nameOfProgrammeOrComment);
+    }
+    
+    else
+    {
+        records.forEach(record => {
+            const li = document.createElement('li');
+            li.append(record.nameOfProgrammeOrComment);
+            const ul = document.querySelector('ul');
+            ul.append(li);
+        }); 
+    }    
+}
 
+fetchUGData();
+
+const fetchPhDData = async() => {
+    
+    const res = await axios.get("https://major-programmes.onrender.com/admissions/majorProgramme/visible?category=PhD Programme");
+    const records = res.data;
+
+        const phdElements = document.querySelectorAll(".PhD")
 
         if(records[0].isComment === true)
         {
-            const p = document.querySelector("p");
-            p.append(records[0].nameOfProgrammeOrComment);
+            phdElements[1].append(records[0].nameOfProgrammeOrComment);
         }
     
         else
@@ -16,19 +40,10 @@ const fetchDdata = async () =>
             records.forEach(record => {
                 const li = document.createElement('li');
                 li.append(record.nameOfProgrammeOrComment);
-                const ul = document.querySelector('ul');
-                ul.append(li);
-            });
-            
-            
+                phdElements[0].append(li);
+            }); 
         }
-    
-    
 
 }
 
-fetchDdata();
-
-
-
-let ul =document.querySelector("ul");
+fetchPhDData();
